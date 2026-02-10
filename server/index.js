97363b58b4aec44db8f90935d3a1a9fb337570dc
vivekyadav-2002 <vivek.yadav@nexby.ai>
2026-02-10 10:47:26 +0530

    port fixed

--- server/index.js
+++ server/index.js
@@ -54,6 +54,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/payment", paymentRoutes);
 
 
+const PORT = process.env.PORT || 5000;
 
 /* ✅ CONNECT DB THEN START SERVER */
 mongoose
@@ -61,8 +62,8 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
